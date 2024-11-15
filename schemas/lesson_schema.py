131c7666c653d48abd4b47c0cb9c9001d3847247
pilakaravi.rs143@gmail.com <pilakaravi.rs143@gmail.com>
2024-11-15 14:18:00 +0530

    inserted cors & Modified DB Schema

--- schemas/lesson_schema.py
+++ schemas/lesson_schema.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import List, Optional
+
+class Subtopic(BaseModel):
+    title: str
+    contentType: str  # 'pdf' in this case
+    content: str  # URL to the PDF
 
 class LessonBase(BaseModel):
-    title: str
-    content: str
+    lessonTitle: str
+    subtopics: List[Subtopic]
 
 class LessonCreate(LessonBase):
     pass
